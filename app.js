c1ddcabb5fbdfe4f348f8071f4b34de157f44f44
Caroline de Oliveira Martins <Caroline de Oliveira Martins>
2016-01-19 21:26:24 -0200

    pages

--- app.js
+++ app.js
@@ -9,9 +9,31 @@
 		$urlRouterProvider.otherwise('/start');
 
 		$stateProvider
+
 			.state('start', {
 				url: '/start',
 				templateUrl: 'start.html'
+			})
+
+			.state('app', {
+				url: '/app',
+				templateUrl: 'app.html'
+			})
+			.state('app.search', {
+				url: '/search',
+				views: {
+					'@app': {
+						templateUrl: 'search.html'
+					}
+				}
+			})
+			.state('app.stalk', {
+				url: '/stalk',
+				views: {
+					'@app': {
+						templateUrl: 'stalk.html'
+					}
+				}
 			});
 	} 
 
